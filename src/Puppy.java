480fa8f91bca73049990e62714dc7ded7713f203
glid1965 <glid1965@gmail.com>
2015-01-26 11:42:26 +0000

    Recommit from Mint13 Setup

---
+++ src/Puppy.java
@@ -0,0 +1,35 @@
+
+public class Puppy{
+	
+	int PuppyAge;
+
+   public Puppy(String name) {
+      // This constructor has one parameter, name.
+      System.out.println("Puppy's Name is: " + name ); 
+   }
+   
+   public void setAge(int age) {
+       PuppyAge = age;
+   }
+
+   public int getAge( ) {
+       System.out.println("Puppy's age is :" + PuppyAge ); 
+       return PuppyAge;
+   }
+
+   
+   public static void main(String []args) {
+      // Create object myPuppy
+      Puppy MyPuppy = new Puppy("Tommy");
+      
+      /* Call class method to set puppy's age */
+      MyPuppy.setAge(2);
+
+      /* Call another class method to get puppy's age */
+      MyPuppy.getAge( );
+
+      /* Access instance variable as well */
+      System.out.println("Variable Value :" + MyPuppy.puppyAge ); 
+
+   }
+}
